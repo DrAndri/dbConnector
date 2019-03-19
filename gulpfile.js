68969da80112889953a20b0c90aaa396ef56de82
root <root@andri.ddns.net>
2019-03-19 00:08:47 +0000

    for new server

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ const gulp = require('gulp');
 const csso = require('gulp-csso');
 const uglify = require('gulp-uglify');
 const autoprefixer = require('gulp-autoprefixer');
+const rename = require('gulp-rename');
 
 // Set the browser that you want to support
 const AUTOPREFIXER_BROWSERS = [
@@ -17,20 +18,30 @@ const AUTOPREFIXER_BROWSERS = [
 ];
 
 gulp.task('scripts', function() {
-  return gulp.src('./html/*.js')
+  return gulp.src('./html/**/*.js')
     // Minify the file
     .pipe(uglify())
+    .pipe(rename({
+            suffix: '.min'
+        }))
     // Output
-    .pipe(gulp.dest('./html/'))
+    .pipe(gulp.dest(function(file) {
+            return file.base;
+        }))
 });
 
 // Gulp task to minify CSS files
 gulp.task('styles', function () {
-  return gulp.src('./html/*.css')
+  return gulp.src('./html/**/*.css')
     // Auto-prefix css styles for cross browser compatibility
     .pipe(autoprefixer({browsers: AUTOPREFIXER_BROWSERS}))
     // Minify the file
     .pipe(csso())
+    .pipe(rename({
+            suffix: '.min'
+        }))
     // Output
-    .pipe(gulp.dest('./html/'))
+    .pipe(gulp.dest(function(file) {
+            return file.base;
+        }))
 });
